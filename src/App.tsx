b93a1a673841a1b602fb9019b4dd971ca012cff0
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2023-10-26 18:29:16 +0400

    space-tourism-website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import Destination from "./components/Destination";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+      </>
+    )
+  );
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="px-6 pt-5">
+      <Header />
+      <RouterProvider router={router} />
     </div>
   );
 }

aa51e16927e6604b0765bd69cd8449df88a8020b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-28 17:24:10 +0400

    almostCompleteDestinationPage

--- src/components/Data.ts
+++ src/components/Data.ts
@@ -113,3 +113,5 @@ const Data = {
     },
   ],
 };
+
+export default Data;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#0B0D17",
+        veryLightBlue: "#D0D6F9",
+        white: "#fff",
+      },
+    },
   },
   plugins: [],
-}
+};

b1b74b58e193a3e7e75d2cb4533f068cbcba9d4e
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-30 18:05:00 +0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,104 @@ import close from "../assets/shared/icon-close.svg";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
+// const Header = () => {
+//   const [mobile, setMobile] = useState(false);
+//   console.log(mobile);
+
+//   return (
+//     <header className="flex justify-between items-center">
+//       <img className="" src={logo} alt="space logo" />
+//       <img
+//         className="md:hidden"
+//         src={menu}
+//         alt="hamburger menu"
+//         onClick={() => setMobile(true)}
+//       />
+//       {mobile && (
+//         <nav
+//           className="absolute
+//        h-full top-0 bottom-0
+//        right-0
+//        backdrop-blur-[40.774227142333984px]
+//        md:bg-[#151823] lg:bg-transparent
+//        pt-5 gap-[65px] flex items-center
+//        flex-col md:static md:block md:h-auto md:px-12 md:pt-0 z-10"
+//         >
+//           <img
+//             className="self-end md:hidden pr-[26.5px]"
+//             src={close}
+//             alt="close icon"
+//             onClick={() => setMobile(false)}
+//           />
+//           <ul className="text-white flex flex-col gap-8 md:flex-row md:gap-9">
+//             <li className="pl-8 md:pl-0">
+//               <NavLink
+//                 to="/"
+//                 state="home"
+//                 className="tracking-[2.7px] md:pr-0 md:py-10 w-full block"
+//               >
+//                 <span className="pr-[11px] md:hidden">00</span>HOME
+//               </NavLink>
+//             </li>
+//             <li className="pl-8 md:pl-0">
+//               <NavLink
+//                 to="/destination"
+//                 state="destination"
+//                 className="tracking-[2.7px] pr-[95px] w-full block md:pr-0 md:py-10"
+//               >
+//                 <span className="pr-[11px] md:hidden">01</span>DESTINATION
+//               </NavLink>
+//             </li>
+//             <li className="pl-8 md:pl-0">
+//               <NavLink
+//                 to="/crew"
+//                 state="crew"
+//                 className="tracking-[2.7px] w-full block md:pr-0 md:py-10"
+//               >
+//                 <span className="pr-[11px] md:hidden">02</span>CREW
+//               </NavLink>
+//             </li>
+//             <li className="pl-8 md:pl-0">
+//               <NavLink
+//                 to="/technology"
+//                 state="technology"
+//                 className="tracking-[2.7px] w-full block md:pr-0 md:py-10"
+//               >
+//                 <span className="pr-[11px] md:hidden">03</span>TECHNOLOGY
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </nav>
+//       )}
+//     </header>
+//   );
+// };
+
+// export default Header;
+
 const Header = () => {
   const [mobile, setMobile] = useState(false);
+  console.log(mobile);
+
   return (
     <header className="flex justify-between items-center">
-      <img className="" src={logo} alt="space logo" />
-      <img
-        className="md:hidden"
-        src={menu}
-        alt="hamburger menu"
-        onClick={() => setMobile(true)}
-      />
-      {mobile && (
+      <img className="md:pl-6 lg:pl-40" src={logo} alt="space logo" />
+      {mobile ? (
+        <img
+          className="md:hidden z-40 cursor-pointer"
+          src={close}
+          alt="close icon"
+          onClick={() => setMobile(false)}
+        />
+      ) : (
+        <img
+          className="md:hidden z-40 cursor-pointer"
+          src={menu}
+          alt="hamburger menu"
+          onClick={() => setMobile(true)}
+        />
+      )}
+      {mobile ? (
         <nav
           className="absolute
        h-full top-0 bottom-0
@@ -23,15 +109,58 @@ const Header = () => {
        backdrop-blur-[40.774227142333984px]
        md:bg-[#151823] lg:bg-transparent
        pt-5 gap-[65px] flex items-center
-       flex-col md:static md:block md:h-auto md:px-12 md:pt-0 z-10"
+       flex-col md:static  md:h-auto md:px-12 md:pt-0 z-10"
         >
-          <img
-            className="self-end md:hidden pr-[26.5px]"
-            src={close}
-            alt="close icon"
-            onClick={() => setMobile(false)}
-          />
-          <ul className="text-white flex flex-col gap-8 md:flex-row md:gap-9">
+          <ul className="text-white flex flex-col gap-8 md:flex-row md:gap-9 pt-32 md:pt-0">
+            <li className="pl-8 md:pl-0">
+              <NavLink
+                to="/"
+                state="home"
+                className="tracking-[2.7px] md:pr-0 md:py-10 w-full block"
+              >
+                <span className="pr-[11px] md:hidden">00</span>HOME
+              </NavLink>
+            </li>
+            <li className="pl-8 md:pl-0">
+              <NavLink
+                to="/destination"
+                state="destination"
+                className="tracking-[2.7px] pr-[95px] w-full block md:pr-0 md:py-10"
+              >
+                <span className="pr-[11px] md:hidden">01</span>DESTINATION
+              </NavLink>
+            </li>
+            <li className="pl-8 md:pl-0">
+              <NavLink
+                to="/crew"
+                state="crew"
+                className="tracking-[2.7px] w-full block md:pr-0 md:py-10"
+              >
+                <span className="pr-[11px] md:hidden">02</span>CREW
+              </NavLink>
+            </li>
+            <li className="pl-8 md:pl-0">
+              <NavLink
+                to="/technology"
+                state="technology"
+                className="tracking-[2.7px] w-full block md:pr-0 md:py-10"
+              >
+                <span className="pr-[11px] md:hidden">03</span>TECHNOLOGY
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <nav
+          className="absolute
+       h-full top-0 bottom-0
+       right-0
+       backdrop-blur-[40.774227142333984px] hidden
+       md:bg-[#151823] lg:bg-transparent
+       pt-5 gap-[65px] items-center
+       flex-col md:static md:flex md:h-auto md:px-12 md:pt-0 z-10"
+        >
+          <ul className="text-white  flex-col gap-8 hidden md:flex md:flex-row md:gap-9 pt-32 md:pt-0">
             <li className="pl-8 md:pl-0">
               <NavLink
                 to="/"
